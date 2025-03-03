db30558012dedfe683ce6ecb2df6eaa4c0cb1b65
kumar7ashutosh <kumarashutoshbtech2023@gmail.com>
2025-03-03 23:54:47 +0530

    hyperparameter tuning

--- src/utils.py
+++ src/utils.py
@@ -17,17 +17,19 @@ def save_object(file_path, obj):
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
-            dill.dump(obj, file_obj)
+            pickle.dump(obj, file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(x_train,y_train,x_test,y_test,models):
+def evaluate_models(x_train,y_train,x_test,y_test,models,param):
     try:
         report={}
         for i in range(len(list(models))):
             model=list(models.values())[i]
-            model.fit(x_train,y_train)
+            para=param[list(models.keys())[i]]
+            gs=GridSearchCV(model,para,cv=3)
+            gs.fit(x_train,y_train)
             y_train_pred=model.predict(x_train)
             y_test_pred=model.predict(x_test)
             train_model_score=r2_score(y_train,y_train_pred)
